28cac4b07417d57ae9673049a6182a44dc87ee55
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2026-01-23 18:24:23 -0500

    Add account_cryptocurrency module for v18

---
+++ addons/account_cryptocurrency/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Account Cryptocurrency",
+    "summary": "Flag currencies as cryptocurrencies and store crypto symbols.",
+    "version": "18.0.1.0.0",
+    "category": "Accounting",
+    "author": "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/res_currency_views.xml",
+    ],
+    "installable": True,
+}
