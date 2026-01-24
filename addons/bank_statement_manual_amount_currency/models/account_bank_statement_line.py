b2f0f7da3373301797118c9094bc2cafc2072887
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2026-01-23 20:11:24 -0500

    Update account_bank_statement_line.py

--- addons/bank_statement_manual_amount_currency/models/account_bank_statement_line.py
+++ addons/bank_statement_manual_amount_currency/models/account_bank_statement_line.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-
 import logging
-
 from lxml import etree
 
 from odoo import api, models
@@ -12,26 +10,6 @@ _logger = logging.getLogger(__name__)
 class AccountBankStatementLine(models.Model):
     _inherit = "account.bank.statement.line"
 
-    @api.model
-    def _get_view(self, view_id=None, view_type="form", **options):
-        arch, view = super()._get_view(view_id, view_type, **options)
-        if view_type == "list":
-            if arch.xpath("//field[@name='amount_currency']"):
-                return arch, view
-            amount_node = arch.xpath("//field[@name='amount']")
-            if amount_node:
-                amount_node[0].addnext(etree.Element("field", name="amount_currency"))
-                amount_node[0].addnext(etree.Element("field", name="foreign_currency_id"))
-            return arch, view
-        if view_type == "form":
-            if arch.xpath("//field[@name='amount_currency']"):
-                return arch, view
-            group_nodes = arch.xpath("//group")
-            if group_nodes:
-                group_nodes[0].append(etree.Element("field", name="foreign_currency_id"))
-                group_nodes[0].append(etree.Element("field", name="amount_currency"))
-            return arch, view
-        return arch, view
 
     @api.model_create_multi
     def create(self, vals_list):
