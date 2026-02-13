084b8b51342b705b765ec26e2e075e675e3dcaf5
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2026-02-13 13:24:45 -0500

    [IMP] journals_balance_show: migrate module to Odoo 18

---
+++ addons/journals_balance_show/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Journals Balance Show",
+    "version": "18.0.1.0.0",
+    "summary": "Show current journal balance in journals list",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_journal_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
