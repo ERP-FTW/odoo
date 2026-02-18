df474cbc41c87e83578a3dd4a6b0f82d029acd62
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2026-02-17 22:18:39 -0500

    [FIX] payment_cardpointe: enforce Cardpointe payment method and lines

--- payment_cardpointe/models/payment_provider.py
+++ payment_cardpointe/models/payment_provider.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from odoo import api, models
+from odoo import _, api, models
 
 from odoo.addons.payment import setup_provider
 
@@ -19,6 +19,29 @@ class PaymentProvider(models.Model):
             setup_provider(self.env.cr, self.env.registry, 'cardpointe')
         except Exception:
             _logger.exception('[CARDPOINTE] Failed to setup provider during registry hook.')
+        return res
+
+    @api.model
+    def _setup_provider(self, code):
+        """Ensure CardPointe payment method/lines exist so journal can be persisted."""
+        res = super()._setup_provider(code)
+        if code != 'cardpointe':
+            return res
+
+        payment_method = self.env['account.payment.method'].search([('code', '=', code)], limit=1)
+        if not payment_method:
+            payment_method = self.env['account.payment.method'].sudo().create({
+                'name': _('Cardpointe'),
+                'code': code,
+                'payment_type': 'inbound',
+            })
+
+        providers = self.search([('code', '=', code)])
+        for provider in providers:
+            if provider.journal_id:
+                provider._ensure_payment_method_line(allow_create=True)
+            else:
+                provider._ensure_payment_method_line(allow_create=False)
         return res
 
     @api.depends('code')
